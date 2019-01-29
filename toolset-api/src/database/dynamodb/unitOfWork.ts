0ef2e43d952b1ba643897bf508d200f4f9964825
Laszlo Losonczi <laszlo_losonczi@epam.com>
2019-01-29 18:18:26 +0100

    DB type from env

--- toolset-api/src/database/dynamodb/unitOfWork.ts
+++ toolset-api/src/database/dynamodb/unitOfWork.ts
@@ -5,6 +5,7 @@ import UserRepository from "./repositories/userRepository";
 @injectable()
 export default class UnitOfWork implements IUnitOfWork {
     constructor() {
+        console.log('"Connected" to DynamoDB')
         this.user = new UserRepository();
     }
 
--- toolset-api/src/inversify.config.ts
+++ toolset-api/src/inversify.config.ts
@@ -3,7 +3,12 @@ import { IUnitOfWork } from "./interfaces";
 import MongoUnitOfWork from "./database/mongodb/unitOfWork";
 import DynamoUnitOfWork from './database/dynamodb/unitOfWork';
 
+const unitOfWorks: any = {
+    mongodb: () => new MongoUnitOfWork(),
+    dynamodb: () => new DynamoUnitOfWork()
+}
+
 const container = new Container({ autoBindInjectable: true });
-container.bind<IUnitOfWork>('unitOfWork').to(DynamoUnitOfWork).inSingletonScope();
+container.bind<IUnitOfWork>('unitOfWork').toDynamicValue(() => unitOfWorks[process.env.DB_TYPE || '']()).inSingletonScope();;
 
 export default container;
