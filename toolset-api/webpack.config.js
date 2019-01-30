5bab2e250cea12bbe3bddbac99133179e624f97d
Laszlo Losonczi <laszlo_losonczi@epam.com>
2019-01-30 17:57:22 +0100

    Production build option

--- toolset-api/webpack.config.js
+++ toolset-api/webpack.config.js
@@ -1,36 +1,38 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-    entry: './src/server.ts',
-    target: 'node',
-    devtool: 'source-map',
-    externals: {
-        express: 'commonjs express',
-        'body-parser': 'commonjs body-parser',
-        mongoose: 'commonjs mongoose',
-        bcryptjs: 'commonjs bcryptjs',
-        jsonwebtoken: 'commonjs jsonwebtoken',
-        cors: 'commonjs cors',
-        inversify: 'commonjs inversify',
-        'reflect-metadata': 'commonjs reflect-metadata',
-    },
-    module: {
-        rules: [{
-            test: /.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }, ],
-    },
-    mode: 'development',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new Dotenv()
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'server.js',
-    },
+module.exports = (env, options) => {
+    return {
+        entry: './src/server.ts',
+        target: 'node',
+        devtool: options.mode !== 'production' ? 'source-map' : '',
+        externals: {
+            express: 'commonjs express',
+            'body-parser': 'commonjs body-parser',
+            mongoose: 'commonjs mongoose',
+            bcryptjs: 'commonjs bcryptjs',
+            jsonwebtoken: 'commonjs jsonwebtoken',
+            cors: 'commonjs cors',
+            inversify: 'commonjs inversify',
+            'reflect-metadata': 'commonjs reflect-metadata',
+        },
+        module: {
+            rules: [{
+                test: /.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },],
+        },
+        mode: options.mode || 'development',
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        plugins: [
+            new Dotenv()
+        ],
+        output: {
+            path: path.join(__dirname, options.mode === 'production' ? 'build' : 'dist'),
+            filename: 'server.js',
+        },
+    };
 };
