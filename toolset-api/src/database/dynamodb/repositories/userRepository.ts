db831687decab83abcae94e380ad95ecb6b1baa6
Laszlo Losonczi <laszlo_losonczi@epam.com>
2019-01-31 18:35:58 +0100

    DynamoDB connection

--- toolset-api/src/database/dynamodb/repositories/userRepository.ts
+++ toolset-api/src/database/dynamodb/repositories/userRepository.ts
@@ -1,12 +1,27 @@
 import { IUserRepository } from "../../../interfaces";
 import { User } from "../../../models/entities/user";
+import { DynamoDB } from "aws-sdk";
 
 export default class UserRepository implements IUserRepository {
-    async createUser(user: User): Promise<void> {
+    constructor(private dynamoDb: DynamoDB.DocumentClient) { }
 
+    async createUser(user: User): Promise<void> {
+        await this.dynamoDb.put({
+            TableName: 'Users',
+            Item: user,
+            ConditionExpression: 'attribute_not_exists(username)'
+        }).promise()
     }
     async getUserByUsername(username: string): Promise<User | null> {
-        return null;
+        const users = (await this.dynamoDb.query({
+            TableName: 'Users',
+            KeyConditionExpression: 'username = :value',
+            ExpressionAttributeValues: {
+                ':value': username
+            }
+        }).promise()).Items || [];
+
+        return users[0] as User;
     }
 
 
--- toolset-api/src/database/dynamodb/unitOfWork.ts
+++ toolset-api/src/database/dynamodb/unitOfWork.ts
@@ -1,12 +1,16 @@
 import { IUnitOfWork } from "../../interfaces";
 import { injectable } from 'inversify';
 import UserRepository from "./repositories/userRepository";
+import { DynamoDB } from 'aws-sdk';
 
 @injectable()
 export default class UnitOfWork implements IUnitOfWork {
     constructor() {
-        console.log(`"Connected" to DynamoDB: ${process.env.DYNAMO_URL}`)
-        this.user = new UserRepository();
+        const dynamoDb = new DynamoDB.DocumentClient({
+            endpoint: process.env.DYNAMO_URL
+        })
+        console.log(`Connected to DynamoDB: ${process.env.DYNAMO_URL}`)
+        this.user = new UserRepository(dynamoDb);
     }
 
     public user: UserRepository;
