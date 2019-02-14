55ca9d46fe3c9f32b8b736f36b0f7be8eed93b4d
Laszlo Losonczi <laszlo_losonczi@epam.com>
2019-02-15 00:02:53 +0100

    API unit tests

---
+++ toolset-api/src/controllers/usersController.spec.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import { expect } from 'chai';
+import 'mocha';
+import UsersController from './usersController';
+import { IUnitOfWork } from '../interfaces';
+
+describe('Users controller', () => {
+    let controller: UsersController;
+
+    beforeEach(() => {
+        controller = new UsersController({} as IUnitOfWork);
+    });
+
+    describe('Login action', () => {
+        it('should return return BadRequest if no username is provided', async () => {
+            const response = await controller.login()({
+                body: {
+                    username: '',
+                    password: 'password123',
+                },
+            });
+
+            expect(response.statusCode).to.equal(400);
+        });
+    });
+});
--- toolset-api/src/models/types/controllerRequest.ts
+++ toolset-api/src/models/types/controllerRequest.ts
@@ -2,7 +2,7 @@ import { IncomingHttpHeaders } from 'http';
 
 export interface ControllerRequest<B, P = undefined, Q = undefined, H = IncomingHttpHeaders> {
     body: B;
-    params: P;
-    query: Q;
-    headers: H;
+    params?: P;
+    query?: Q;
+    headers?: H;
 }
